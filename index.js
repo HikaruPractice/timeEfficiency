96df8f09366f5cf078090496554b8695862027c2
HikaruPractice <119712253+HikaruPractice@users.noreply.github.com>
2023-04-25 20:11:38 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ let stopTime=null;
 let restartTime=null;
 let executionTime=new Date(startingTimeSerial);
 let times=0;
-let ave=new Date(startingTimeSerial);
-let perMinute=0;
-let perHour=0;
 let breaking = false;
 let nowBreakTime=new Date(startingTimeSerial);
 let totalBreakTime=new Date(startingTimeSerial);
@@ -26,7 +23,10 @@ function start(){
     document.getElementById("countDisplay").disabled=false;
     document.getElementById("countMinus").disabled=false;
     startTime = new Date();
+    breaking=false;
+    localSave_ifChecked();
     intervalID=setInterval(update,500);
+
 }
 
 function restart(){
@@ -40,6 +40,7 @@ function restart(){
     totalBreakTime = timeAdd(totalBreakTime,preBreakTime);
     allBreakTime.setTime(totalBreakTime.getTime());
     breaking=false;
+    localSave_ifChecked();
 }
 
 
@@ -55,9 +56,10 @@ function fin(){
     update();
     
     breaking=true;
+    localSave_ifChecked();
 }
 
-function reset(){
+function reset(loadmode=false){
     document.getElementsByClassName("start")[0].style.display="block";
     document.getElementsByClassName("restart")[0].style.display="none";
     document.getElementsByClassName("stop")[0].style.display="none";
@@ -65,8 +67,10 @@ function reset(){
     document.getElementsByClassName("reset")[0].disabled = false;
     document.getElementById("countDisplay").disabled=true;
     document.getElementById("countMinus").disabled=true;
+    if (intervalID!==""){
     clearInterval(intervalID);
     intervalID="";
+    }
     breaking=false;
 
     startTime=null;
@@ -76,9 +80,6 @@ function reset(){
     restartTime=null;
     executionTime.setTime(startingTimeSerial);
     times=0;
-    ave=0;
-    perMinute=0;
-    perHour=0;
     nowBreakTime.setTime(startingTimeSerial);
     totalBreakTime.setTime(startingTimeSerial);
     allBreakTime.setTime(startingTimeSerial);
@@ -90,7 +91,9 @@ function reset(){
     document.getElementsByClassName('countDisplay')[0].textContent=0;
     document.getElementById('perMinute').textContent=0;
     document.getElementById('perHour').textContent=0;
-
+    if (!loadmode){
+        localRemove();
+    }
 }
 
 
@@ -146,31 +149,57 @@ function timesAdd(n){
     setAverage();
     setPerHour();
     }
+    localSave_ifChecked();
 }
 
 function setAverage(){
-    
+    let ave;
+    let ave2;
     if (toString.call(operatingTime) !== '[object Date]'){
         return;
     }
     if (times<=0){
         document.getElementById('averageTime').textContent=0;
-        ave.setTime(startingTimeSerial);
+        ave= new Date(startingTimeSerial);
         return;
     }
     let aveTimeSerial
     aveTimeSerial=(operatingTime.getTime()-startingTimeSerial)/times+startingTimeSerial;
-    ave.setTime(aveTimeSerial);
+    ave=new Date(aveTimeSerial);
     let timeArr=timeToArr(ave);
     let text;
     if (timeArr[3]>=1){
         text=timeArr[3]+"h"+( '00' + timeArr[2] ).slice( -2 )+"m";
     }else if(timeArr[2]>=1){
-        text=( '00' + timeArr[2] ).slice( -2 )+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
+        text=timeArr[2]+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
     }    else{
-        text=( '00' + timeArr[1] ).slice( -2 )+"s";
+        text=timeArr[1]+"s";
     }
     document.getElementById('averageTime').textContent=text;
+
+    if (toString.call(executionTime) !== '[object Date]'){
+        return;
+    }
+    if (times<=0){
+        document.getElementById('averageTime').textContent=0;
+        ave2= new Date(startingTimeSerial);
+        return;
+    }
+    let aveTimeSerial2
+    aveTimeSerial2=(executionTime.getTime()-startingTimeSerial)/times+startingTimeSerial;
+    ave2=new Date(aveTimeSerial2);
+    timeArr=timeToArr(ave2);
+    let text2;
+    if (timeArr[3]>=1){
+        text2=timeArr[3]+"h"+( '00' + timeArr[2] ).slice( -2 )+"m";
+    }else if(timeArr[2]>=1){
+        text2=timeArr[2]+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
+    }    else{
+        text2=timeArr[1]+"s";
+    }
+    document.getElementById('averageTime2').textContent=text2;
+
+
 }
 
 function setPerHour(){
@@ -182,25 +211,56 @@ function setPerHour(){
     }
     let minute = (operatingTime.getTime()-startingTimeSerial)/1000/60;
     let hour = minute/60;
-    perMinute = times/minute;
-    perHour = times/hour;
+    let perMinute = times/minute;
+    let perHour = times/hour;
     document.getElementById('perMinute').textContent=Math.floor(perMinute*10)/10;
     document.getElementById('perHour').textContent=Math.floor(perHour*10)/10;
+
+    
+    if (typeof executionTime === 'undefined'){
+        return;
+    }
+    if (times<=0){
+        document.getElementById('perHour2').textContent=0;
+    }
+    let minute2 = (executionTime.getTime()-startingTimeSerial)/1000/60;
+    let hour2 = minute2/60;
+    let perMinute2 = times/minute2;
+    let perHour2 = times/hour2;
+    document.getElementById('perMinute2').textContent=Math.floor(perMinute2*10)/10;
+    document.getElementById('perHour2').textContent=Math.floor(perHour2*10)/10;
 }
 function update(){
     nowTime= new Date();
     setTimeDifference(executionTime,startTime,nowTime);
-    //setOperatingTime();
     if (breaking){
         setTimeDifference(nowBreakTime,stopTime,nowTime);
         allBreakTime=timeAdd(totalBreakTime,nowBreakTime);
     }else{
     
-        setOperatingTime();
+    setOperatingTime();
+    }
+    //debug();
     setAverage();
     setPerHour();
-    }
-    debug();
+    displayTime();
+}
+
+function displayTime(){
+    let timeArr;
+    timeArr =timeToArr(startTime);
+    gei('startTime').textContent='開始:'+( '00' + timeArr[3] ).slice( -2 )+"時"+( '00' + timeArr[2] ).slice( -2 )+"分";
+    timeArr =timeToArr(nowTime);
+    gei('nowTime').textContent='時刻:'+( '00' + timeArr[3] ).slice( -2 )+"時"+( '00' + timeArr[2] ).slice( -2 )+"分";
+    timeArr =timeToArr(executionTime);
+    gei('proccec').textContent=( '00' + timeArr[3] ).slice( -2 )+"h"+( '00' + timeArr[2] ).slice( -2 )+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
+    timeArr =timeToArr(allBreakTime);
+    gei('allBreakTime').textContent=( '00' + timeArr[3] ).slice( -2 )+"h"+( '00' + timeArr[2] ).slice( -2 )+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
+
+    let operatingTimeSerial = operatingTime.getTime()-startingTimeSerial;
+    let executionTimeSerial = executionTime.getTime()-startingTimeSerial;
+    let rate=Math.round(operatingTimeSerial*100/executionTimeSerial);
+    gei('operatingRate').textContent=rate+':'+(100-rate)
 }
 
 
@@ -210,8 +270,6 @@ function debug(){
     gei('startTime').textContent=( '00' + timeArr[3] ).slice( -2 )+"時"+( '00' + timeArr[2] ).slice( -2 )+"分"+( '00' + timeArr[1] ).slice( -2 )+"秒";
     timeArr =timeToArr(nowTime);
     gei('nowTime').textContent=( '00' + timeArr[3] ).slice( -2 )+"時"+( '00' + timeArr[2] ).slice( -2 )+"分"+( '00' + timeArr[1] ).slice( -2 )+"秒";
-    timeArr =timeToArr(executionTime);
-    gei('proccec').textContent=( '00' + timeArr[3] ).slice( -2 )+"h"+( '00' + timeArr[2] ).slice( -2 )+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
     timeArr =timeToArr(stopTime);
     gei('breakTime').textContent=( '00' + timeArr[3] ).slice( -2 )+"時"+( '00' + timeArr[2] ).slice( -2 )+"分"+( '00' + timeArr[1] ).slice( -2 )+"秒";
     timeArr =timeToArr(restartTime);
@@ -224,8 +282,6 @@ function debug(){
     gei('nowBreakTime').textContent=( '00' + timeArr[3] ).slice( -2 )+"h"+( '00' + timeArr[2] ).slice( -2 )+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
     timeArr =timeToArr(totalBreakTime);
     gei('totalBreakTime').textContent=( '00' + timeArr[3] ).slice( -2 )+"h"+( '00' + timeArr[2] ).slice( -2 )+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
-    timeArr =timeToArr(allBreakTime);
-    gei('allBreakTime').textContent=( '00' + timeArr[3] ).slice( -2 )+"h"+( '00' + timeArr[2] ).slice( -2 )+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
     timeArr =timeToArr(operatingTime);
     gei('opeTime').textContent=( '00' + timeArr[3] ).slice( -2 )+"h"+( '00' + timeArr[2] ).slice( -2 )+"m"+( '00' + timeArr[1] ).slice( -2 )+"s";
 }
@@ -234,7 +290,40 @@ function gei(id){
     return document.getElementById(id)
 }
 
+function inputStartTime(n = null){
+    if (startTime===null){
+        return;
+    }
+    if (n===null){
+        startTime=new Date();
+    }else{
+        startTime.setMinutes(startTime.getMinutes()+n);
+    }
+    update2();
+    localSave_ifChecked();
+}
+function inputBreakTime(n = null){
+    if (n===null){
+        return;
+    }else{
+        totalBreakTime.setMinutes(totalBreakTime.getMinutes()+n);
+    }
+    update2();
+    localSave_ifChecked();
+}
 
+function inputTimes(){
+    n=gei('setTimes').value;
+    if (isNaN(n)){
+        return;
+    }else{
+        times=Number(n);
+    }
+    timesAdd(0);
+    update2();
+    localSave_ifChecked();
+
+}
 
 // 没　時間単位変換　Date型に100分と指定すると1時40分と解釈される仕様を使えばいい
 
